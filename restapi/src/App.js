28ee67274ca5d6afc8a1e0eaa9385eea2590d306
Marija Vrljanac <marija.vrljanac@gmail.com>
2022-10-17 16:32:58 +0200

    Functionalities added

--- restapi/src/App.js
+++ restapi/src/App.js
@@ -1,54 +1,72 @@
-import React, { Component, useState, useEffect } from 'react';
+import React, { useState, useEffect } from 'react';
+import { Card, Image, Icon } from 'semantic-ui-react';
+import gitLogo from './R.png';
 import './App.css';
 
-class App extends Component {
+function App() {
 
-    state = {
-      loading: true,
-      user: null,
-    };
+  const [avatar_url, setAvatarUrl] = useState("");
+  const [login, setLogin] = useState("");
+  const [userInput, setUserInput] = useState("");
+  const [error, setError] = useState(null);
 
+  const query = "Marija";
 
+  useEffect(() => {
+    fetch(`https://api.github.com/search/users?q=${query}`)
+    .then(res => res.json())
+    .then(data => {
+      setData(data);
+    });
+  }, []);
 
-  async componentDidMount(){
-      const query = "Marija";
-    // const query = document.getElementById("input-field").value;
+  const setData = ({ avatar_url, login }) => {
+    setAvatarUrl(avatar_url);
+    setLogin(login);
+  };
 
-    const response = await fetch(`https://api.github.com/search/users?q=${query}`);
-    const data = await response.json();
-    this.setState({user: data.items[0], loading: false });
-    console.log(data.items[0]);
-  }
-  
-  render (){
-    if (this.state.loading || !this.state.user){
-      return (
-        <div className="container">
-        <div className="image-div">
-            <img src="assets/R.png" alt="git-icon"/>
-        </div>    
-        <input id="input-field" type="text" placeholder="Search for GitHub user"/>
+  const handleSearch = (e) => {
+    setUserInput(e.target.value);
+  };
+
+  const handleSubmit = () => {
+    fetch(`https://api.github.com/search/users?q=${userInput}`)
+    .then(res => res.json())
+    .then(data => {
+      if (data.message) {
+        setError(data.message);
+      }else{
+        setData(data);
+        setError(null);
+      }   
+    })
+  };
+
+  return (
+    <div className="App">
+      <div className='container'>
+        <img src={gitLogo} alt='logo'/>
         <br/>
-    </div>  
-      );
-    }
-    return (
-      <div>
-        
-    <div id="users">
-        <div id="user">
-            <div>
-              <img src={ this.state.user.avatar_url }/>
-              <div>{ this.state.user.login }</div>
-         </div> 
-        </div>    
-      </div>     
-    </div>
-    );
+        <input id="input-field" type="text" placeholder="Search for GitHub user" onChange={ handleSearch } onKeyUp= { handleSubmit }/>
+          <br></br>
 
-    
-  }
-    
+      </div>
+      { error ? (<h1>{error}</h1>) : (
+        <div className='card'>
+        <Card>
+          <Image src={ avatar_url } wrapped ui={false} />
+          <Card.Content>
+            <Card.Header>{login}</Card.Header>
+          </Card.Content>
+          <Card.Content extra>
+            <button className='userDetails'>User Detail</button>
+          </Card.Content>
+       </Card>
+    </div>
+      )}
+      
+    </div>
+  );
 }
 
 export default App;
