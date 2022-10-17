2467bb64ca8e13d26b67da0f61835f8fe8598930
Marija Vrljanac <marija.vrljanac@gmail.com>
2022-10-17 11:09:26 +0200

    API called

--- index.js
+++ index.js
@@ -1,5 +1,41 @@
 const inputField = document.getElementById("input-field");
 
-inputField.addEventListener("click", function () {
-    alert("Hi!");
+inputField.addEventListener("keyup", function () {
+        getGithubUser();
 });
+
+async function getGithubUser(){
+    const query = document.getElementById("input-field").value;
+
+    if(!query){
+        alert("Please enter a name!");
+        return;
+    }
+
+    const endpoint = new URL(`https://api.github.com/search/users?q=${query}`);
+
+
+    // endpoint.searchParams.set("token", "YOUR_TOKEN_HERE");
+
+    const response = await fetch(endpoint);
+
+    // const response = await fetch(endpoint, {
+    //     headers: {
+    //         "Authorization":  "YOUR_TOKEN_HERE"
+    //     }
+    // });
+
+    if(response.status === 404) {
+        alert("Username not found!");
+        return;
+    }
+
+    const data = await response.json();
+
+    // alert(`Total count: ${data.total_count}\nIncomplete results: ${data.incomplete_results}\nItems: ${data.items || "N/A"}`);
+
+    console.log(response);
+    console.log(data);
+
+
+}
