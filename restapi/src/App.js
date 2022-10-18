9f43ff6f9c3668fb66ef9d3e5fe52d326188141d
Marija Vrljanac <marija.vrljanac@gmail.com>
2022-10-18 09:37:09 +0200

    HomePage and User components included in App

--- restapi/src/App.js
+++ restapi/src/App.js
@@ -1,71 +1,16 @@
-import React, { useState, useEffect } from 'react';
-import { Card, Image, Icon } from 'semantic-ui-react';
-import gitLogo from './R.png';
 import './App.css';
+import HomePage from "./components/HomePage";
+import User from "./components/User";
+import { BrowserRouter, Routes, Route } from "react-router-dom"
 
 function App() {
-
-  const [avatar_url, setAvatarUrl] = useState("");
-  const [login, setLogin] = useState("");
-  const [userInput, setUserInput] = useState("");
-  const [error, setError] = useState(null);
-
-  // const query = "Marija";
-
-  useEffect(() => {
-    fetch(`https://api.github.com/search/users?q=${userInput}`)
-    .then(res => res.json())
-    .then(data => {
-      setData(data);
-    });
-  }, []);
-
-  const setData = ({ avatar_url, login }) => {
-    setAvatarUrl(avatar_url);
-    setLogin(login);
-  };
-
-  const handleSearch = (e) => {
-    setUserInput(e.target.value);
-  };
-
-  const handleSubmit = () => {
-    fetch(`https://api.github.com/search/users?q=${userInput}`)
-    .then(res => res.json())
-    .then(data => {
-      if (data.message) {
-        setError(data.message);
-      }else{
-        setData(data);
-        setError(null);
-      }   
-    })
-  };
-
   return (
-    <div className="App">
-      <div className='container'>
-        <img src={gitLogo} alt='logo'/>
-        <br/>
-        <input id="input-field" type="text" placeholder="Search for GitHub user" onChange={ handleSearch } onKeyUp= { handleSubmit }/>
-          <br></br>
-
-      </div>
-      { error ? (<h1>{error}</h1>) : (
-        <div className='card'>
-        <Card>
-          <Image src={ avatar_url } wrapped ui={false} />
-          <Card.Content>
-            <Card.Header>{login}</Card.Header>
-          </Card.Content>
-          <Card.Content extra>
-            <button className='userDetails'>User Detail</button>
-          </Card.Content>
-       </Card>
-    </div>
-      )}
-      
-    </div>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<HomePage/>}/>
+        <Route path="/user" element={<User/>}/>
+      </Routes>
+    </BrowserRouter>
   );
 }
 
--- restapi/src/components/User.js
+++ restapi/src/components/User.js
@@ -1,9 +1,6 @@
 import React from "react";
 import "./User.css";
 // import { useState, useEffect } from 'react';
-import { Link } from "react-router-dom";
-import axios from "../axios";
-import { useNavigate } from 'react-router-dom';
 
 
 const User = ( { user } ) => {
