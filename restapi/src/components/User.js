f1afc9d06c0ffa4224b7ab5652d3a1962fa5a81f
Marija Vrljanac <marija.vrljanac@gmail.com>
2022-10-19 11:00:15 +0200

    Versioning

--- restapi/src/components/User.js
+++ restapi/src/components/User.js
@@ -1,6 +1,7 @@
 import React from "react";
 import "./User.css";
 // import { useState, useEffect } from 'react';
+import { Link } from "react-router-dom";
 
 
 const User = ( { user } ) => {
@@ -12,10 +13,15 @@ const User = ( { user } ) => {
 
     return(
       <div className="card">
-            <img src={ avatar_url } alt="avatar"></img>
-            <p>{ login }</p>
-            {/* <Link to={`/user/${ login }`} className="loginbtn">User Detail</Link>  */}
-            <a href={ html_url }><button className="user-detail-btn">User Detail</button></a>
+      <div className="image">
+        <img src={avatar_url} alt={login} />
+      </div>
+      <div className="user-info">
+        <h4>{login}</h4>
+      </div>
+      <div className="user-detail">
+        <Link to={`/user/${login}`}><button className="user-detail-btn">User Detail</button></Link>
+        </div>
     </div>
     )
 };
