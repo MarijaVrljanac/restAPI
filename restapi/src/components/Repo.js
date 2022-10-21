d57dac09e452b3f06ba9d9ce5176b2199798c37d
Marija Vrljanac <marija.vrljanac@gmail.com>
2022-10-21 15:12:08 +0200

    Final style changes

--- restapi/src/components/Repo.js
+++ restapi/src/components/Repo.js
@@ -7,16 +7,29 @@ import { Link } from "react-router-dom";
 const Repo = ( { repo } ) => {
 
     const { name, html_url, description, language } = repo;
+    const [isShown, setIsShown] = useState(false);
+
+    const descButton = document.querySelector(".descBtn");
+
+    const toggleDesc = event => {
+        setIsShown(current => !current);
+      };
 
     return(
         <div className="repo">
             <h3 className="repo-name">
                 <a href={html_url} >{name}</a>
             </h3>
-            {/* <button type="button" className="descBtn" onClick={ toggle }>Read more...</button> */}
-                {/* <p className="desc">
+            {description && <button type="button" className="descBtn" onClick={ toggleDesc }> {isShown ? "Hide description..." : "Show description..."} </button>
+           }
+            {isShown && (
+            <div>
+                <p className="desc">
                     {description}
-                </p> */}
+                </p>
+            </div>
+             )}
+                
         { language && <small className="language">• {language}</small>}
     </div>
     )
